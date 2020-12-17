4c17c02096b05219a44997eb01078198c7a9f306
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-17 22:17:10 +0530

    added auther schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,14 +4,21 @@ const _ = require("lodash");
 const { 
   GraphQLObjectType, 
   GraphQLString, 
-  GraphQLSchema ,
-  GraphQLID
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
 } = graphql;
 
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1",authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2",authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3",authorId: "3" },
+];
+
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' }
 ];
 
 const BookType = new GraphQLObjectType({
@@ -26,6 +33,28 @@ const BookType = new GraphQLObjectType({
     genre: {
       type: GraphQLString,
     },
+    author:{
+      type: AuthorType,
+      resolve(parent,args){
+        console.log(parent);
+        return _.find(authors,{ id:parent.authorId})
+      }
+    }
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
+    },
   }),
 });
 
@@ -36,11 +65,17 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        console.log(typeof(args.id));
         return _.find(books, { id: args.id });
       },
     },
-  },
+    author:{
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({
