78cfda0b012a823f6f28ef7d82c776578476bc0a
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-17 22:00:37 +0530

    first commit

---
+++ server/schema/schema.js
@@ -0,0 +1,48 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { 
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema ,
+  GraphQLID
+} = graphql;
+
+var books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    genre: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        console.log(typeof(args.id));
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
