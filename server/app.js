52c4d44b03b21574f3e8b192bd1aed3d845a8a70
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-17 23:02:24 +0530

    connected to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://king:9994532266@node-netninja.4r9my.mongodb.net/test",{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connection.once('open',()=>{
+    console.log("Connected to database");
+})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
