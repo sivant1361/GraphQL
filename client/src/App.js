dd36e909e9c22b66e5432d34512d89192b6f974e
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-18 16:47:51 +0530

    created book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <h1>AUTHOR AND BOOKS</h1>
+      <BookList/>
+      <BookList/>
+      <BookList/> 
     </div>
   );
 }
-
-export default App;
