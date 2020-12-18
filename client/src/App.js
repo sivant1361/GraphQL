053a5fba590f34ea1e6797082d85e3991a77e70c
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2020-12-18 16:57:05 +0530

    installed apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import BookList from './components/BookList'
+import BookList from "./components/BookList";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <div id="main">
-      <h1>AUTHOR AND BOOKS</h1>
-      <BookList/>
-      <BookList/>
-      <BookList/> 
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>AUTHOR AND BOOKS</h1>
+        <BookList />
+        <BookList />
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
